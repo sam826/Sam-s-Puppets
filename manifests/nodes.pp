a089323aeb51a4d8c4490ded08bf484ab7976fce
Sam 826 <sam826@gmail.com>
2012-02-23 05:20:16 -0500

    added apache vhost definitions

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -9,6 +9,22 @@ node 'vm-pc-ub-1.nyc.rr.com'{
 
 node 'vm-pc-ub-2.nyc.rr.com'{
 	include base
+	include apache
+
+	apache::vhost { 'www.example.com':
+	  port => 80,
+	  docroot => '/var/www/www.example.com',
+	  ssl => false,
+	  priority => 10,
+	  serveraliases => 'home.example.com',
+	}
+
+	apache::vhost { 'another.example.com':
+	  port => 80,
+	  docroot => '/var/www/another.example.com',
+	  ssl => false,
+	  priority => 10,
+	}
 }
 
 node 'vm-pc-ub-3.nyc.rr.com'{
